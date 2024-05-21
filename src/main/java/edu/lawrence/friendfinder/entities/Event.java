a1c75164fbef7342d191131b5e415f42e754618c
Henry Roach <129221637+hankwr@users.noreply.github.com>
2024-05-21 11:26:47 -0500

    Added Event.java

---
+++ src/main/java/edu/lawrence/friendfinder/entities/Event.java
@@ -0,0 +1,96 @@
+package edu.lawrence.friendfinder.entities;
+
+// Java-level includes [Time]
+import java.time.Instant;
+
+// Jakarta-level includes [Class Annotations]
+import jakarta.persistence.Entity;
+
+// Jakarta-level includes [Field Annotations]
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Event{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@ManyToOne
+	@JoinColumn(name = "user")
+	private User host;
+	
+	private String name;
+	private String description;
+	private String location;
+	
+	private Instant startTime;
+	private Instant endTime;
+	
+	// Genre and Platform tags not set up for events yet
+//	private List<GenreTag> genres;
+//	private List<PlatformTag> platforms;
+	
+	public Event() {}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public User getHost() {
+		return host;
+	}
+
+	public void setHost(User host) {
+		this.host = host;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Instant getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Instant startTime) {
+		this.startTime = startTime;
+	}
+
+	public Instant getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Instant endTime) {
+		this.endTime = endTime;
+	}
+	
+}
