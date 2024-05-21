e7ada0023983e1557a27264e7edc8d37208f248b
Henry Roach <129221637+hankwr@users.noreply.github.com>
2024-05-21 16:45:22 -0500

    Built out and fixed conversion features

--- src/main/java/edu/lawrence/friendfinder/services/ConvertTimeService.java
+++ src/main/java/edu/lawrence/friendfinder/services/ConvertTimeService.java
@@ -2,6 +2,7 @@ package edu.lawrence.friendfinder.services;
 
 // Java-level includes [Time]
 import java.time.Instant;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class ConvertTimeService {
@@ -12,14 +13,52 @@ public class ConvertTimeService {
 	 * 11:59 am, Thu 05/16/2024 PST
 	 */
 	private static final String pattern = "hh:mm a, EEE MM:dd:yyyy z";
-	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern).withZone(ZoneId.of("UTC"));
 	
+	/**
+	 * Formats a java.time.Instant into a String, assumes UTC timezone
+	 * @param time the Instant object to convert
+	 * @return the formatted String version of the instant
+	 */
 	static public String InstantToString(Instant time) {
-		return formatter.format(time);
+		String ret = formatter.format(time);
+	
+		return ret;
 	}
 	
+	/**
+	 * Formats a java.time.Instant into a String
+	 * @param time time the Instant object to convert
+	 * @param zone the specified timezone to display in the String
+	 * @return the formatted String version of the instant
+	 */
+	static public String InstantToString(Instant time, String zone) {
+		String ret = formatter.withZone(ZoneId.of(zone, ZoneId.SHORT_IDS)).format(time);
+		
+		return ret;
+	}
+	
+	/**
+	 * Parses a formatted String into a java.time.Instant, formats to UTC timezone
+	 * @param time the formatted String representation of time (hh:mm a, MM:dd:yyyy z)
+	 * @return the parsed Instant object
+	 */
 	static public Instant StringToInstant(String time) {
-		return Instant.from(formatter.parse(time));
+		Instant ret = Instant.from(formatter.parse(time));
+		
+		return ret;
+	}
+	
+	/**
+	 * Parses a formatted String into a java.time.Instant, formats to specified timezone
+	 * @param time the formatted String representation of time (hh:mm a, MM:dd:yyyy z)
+	 * @param zone the specified zone abbreviation (UTC, CST, PST, etc.)
+	 * @return the parsed Instant object
+	 */
+	static public Instant StringToInstant(String time, String zone) {
+		Instant ret = Instant.from(formatter.withZone(ZoneId.of(zone, ZoneId.SHORT_IDS)).parse(time));
+		
+		return ret;
 	}
 	
 }
