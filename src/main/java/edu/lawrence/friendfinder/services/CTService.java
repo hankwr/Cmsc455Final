7cda6c5140dafbf02a7e72cfaaf65fe06a1fe97b
Henry Roach <129221637+hankwr@users.noreply.github.com>
2024-05-22 00:01:13 -0500

    Improved internal consistency, fixed regex bug

--- src/main/java/edu/lawrence/friendfinder/services/CTService.java
+++ src/main/java/edu/lawrence/friendfinder/services/CTService.java
@@ -21,7 +21,11 @@ public class CTService {
 	 * leading zeros in front of hour will be removed automatically
 	 */
 	private static final String pattern = "h:mm a, M:dd:yyyy z";
+	private static final String regexPattern = "^\\d{1,2}:\\d{2} [A-Z]{2}, \\d{1,2}/\\d{2}/\\d{4} [A-Z]{1,3}$";
 	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern).withZone(ZoneId.of("UTC"));
+	private static final String formattedEpoch = formatter.format(Instant.EPOCH);
+	
+	
 	
 	/**
 	 * Formats a java.time.Instant into a String (h:mm a, M/dd/yyyy z), assumes UTC timezone
@@ -35,7 +39,7 @@ public class CTService {
 		ret = formatter.format(time);
 		}catch (Exception e) {
 			e.printStackTrace();
-			return "12:00 AM, 1/01/1970 UTC";
+			return formattedEpoch;
 		}
 		
 		return ret;
@@ -54,7 +58,7 @@ public class CTService {
 			ret = formatter.withZone(ZoneId.of(zone, ZoneId.SHORT_IDS)).format(time);
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "12:00 AM, 1/01/1970 UTC";
+			return formattedEpoch;
 		}
 		
 		return ret;
@@ -63,14 +67,14 @@ public class CTService {
 	/**
 	 * Parses a formatted String into a java.time.Instant, formats to UTC timezone
 	 * @param time the formatted String representation of time (h:mm a, M/dd/yyyy z)
-	 * @return the parsed Instant object, returns EPOCH if failed
+	 * @return the parsed Instant object, returns EPOCH date if failed
 	 */
 	static public Instant strToInst(String time) {
 		Instant ret;
 		
 		boolean status = false;
 		try { // My intellisense freaks out about unreachable code without this tryCatch block
-			status = time.matches("^\\d{1,2}:\\d{2}:\\d{2} [A-Z]{2}, \\d{1,2}/\\d{2}/\\d{4} [A-Z]{1,3}$");
+			status = time.matches(regexPattern);
 		} catch (PatternSyntaxException e){
 			e.printStackTrace();
 			ret = Instant.EPOCH;
@@ -96,14 +100,14 @@ public class CTService {
 	 * Parses a formatted String into a java.time.Instant, formats to specified timezone
 	 * @param time the formatted String representation of time (h:mm a, M/dd/yyyy z)
 	 * @param zone the specified zone abbreviation (UTC, CST, PST, etc.)
-	 * @return the parsed Instant object, returns EPOCH if failed
+	 * @return the parsed Instant object, returns EPOCH date if failed
 	 */
 	static public Instant strToInst(String time, String zone) {
 		Instant ret;
 		
 		boolean status = false;
 		try { // My intellisense freaks out about unreachable code without this tryCatch block
-			status = time.matches("^\\d{1,2}:\\d{2}:\\d{2} [A-Z]{2}, \\d{1,2}/\\d{2}/\\d{4} [A-Z]{1,3}$");
+			status = time.matches(regexPattern);
 		} catch (PatternSyntaxException e){
 			e.printStackTrace();
 			ret = Instant.EPOCH;
@@ -127,15 +131,15 @@ public class CTService {
 
 	/**
 	 * Checks to see if formatted String timestamp is valid in both format and content
-	 * @param timestamp the input String timestamp to check
+	 * @param time the input String timestamp to check
 	 * @return 
 	 */
-	static public boolean checkFormat(String timestamp) {
+	static public boolean checkFormat(String time) {
 		
 		boolean status;
 		
 		try { // Intellisense freaks out about unreachable code without this tryCatch block
-			status = timestamp.matches("^\\d{1,2}:\\d{2}:\\d{2} [A-Z]{2}, \\d{1,2}/\\d{2}/\\d{4} [A-Z]{1,3}$");
+			status = time.matches(regexPattern);
 		} catch (PatternSyntaxException e){
 			e.printStackTrace();
 			return false;
@@ -145,7 +149,7 @@ public class CTService {
 			return status;
 		
 		try { // Use tryCatch block to verify that Instant is valid (Feb 30th would be invalid, for example)
-			Instant.from(formatter.withResolverStyle(ResolverStyle.STRICT).parse(timestamp));
+			Instant.from(formatter.withResolverStyle(ResolverStyle.STRICT).parse(time));
 		} catch (DateTimeParseException e) {
 			return false;
 		} catch (Exception e) {
