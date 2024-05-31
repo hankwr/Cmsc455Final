9e877bc62feb204679671adb27f6aed0e5726466
Henry Roach <129221637+hankwr@users.noreply.github.com>
2024-05-31 15:44:10 -0500

    Tweaked HQL query

--- src/main/java/edu/lawrence/friendfinder/repositories/RegistrationRepository.java
+++ src/main/java/edu/lawrence/friendfinder/repositories/RegistrationRepository.java
@@ -13,7 +13,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface RegistrationRepository extends JpaRepository<Registration, Integer>{
-	@Query("select r from Registration r where event=:eventid and user=:userid")
+	@Query("select r from Registration r where event.id=:eventid and user.id=:userid")
 	Optional<Registration> checkForDuplicates(Integer eventid, UUID userid);
 	/*
 	List<Registration> findByEvent(Integer eventid);
